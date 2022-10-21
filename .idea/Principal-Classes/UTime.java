3ffb5d71685dc85f230cd0e5850b2e6b159e2620
Levent <lab192@case.edu>
2022-10-21 15:15:47 -0400

    EventTime added. Bare-bones implementation. UTime is now comparable

--- .idea/Principal-Classes/UTime.java
+++ .idea/Principal-Classes/UTime.java
@@ -2,17 +2,23 @@
 //This is just to enforce our conventions for storing and manipulating times
 //Time is stored as UNIX time - the number of seconds since jan 1, 1970
 
-class UTime {
+class UTime implements Comparable {
 
     //The time, stored in seconds since jan 1, 1970
     long time;
 
-    //Initializes time as the current time
+    //Initializes time as the current time]
     public UTime()
     {
         time = System.currentTimeMillis() / 1000;
     }
 
+    public int compareTo(Time other)
+    {
+        long d = duration(this, other);
+        return (d==0 ? 0 : (d>0 ? -1 : 1) );
+
+    }
     //Initializes time as the given value
     public UTime(long _time)
     {
