f6692e2f29bdba00f0d3a7571b1aa7694651c394
davidhfrost <82253398+davidhfrost@users.noreply.github.com>
2022-10-21 15:51:20 -0400

    Began unit tests for Account

--- Backend-Stuff/gamifiedcalendar/gamifiedcalendar/src/test/java/com/swe/gamifiedcalendar/GamifiedCalendarApplicationTests.java
+++ Backend-Stuff/gamifiedcalendar/gamifiedcalendar/src/test/java/com/swe/gamifiedcalendar/GamifiedCalendarApplicationTests.java
@@ -3,6 +3,7 @@ package com.swe.gamifiedcalendar;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.mockito.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -16,8 +17,22 @@ class GamifiedCalendarApplicationTests {
 	}
 
 	@Test
-	void customTests() {
+	void accountTests() {
+		Account a = new Account();
+		String name = "Do homework";
+		a.setTaskname(name);
+		assertEquals(name, a.getTaskName());
 		assertEquals(1, 1);
+		String description = "Java project, chemistry worksheet, physics homework all need to be done";
+		a.setTaskDescription(description);
+		assertEquals(description, a.getTaskDescription());
+		int taskPoints = 5;
+		a.setTaskPoints(taskPoints);
+		assertEquals(taskPoints, a.getTaskPoints());
+		boolean taskStatus = true;
+		a.setTaskStatus(taskStatus);
+		assertEquals(taskStatus, a.getTaskStatus());
+
 	}
 
 }
