04848b60b0104a5d3496be1578ed64e025e6714c
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2022-10-22 13:47:00 -0400

    Implement Account class

--- Backend-Stuff/gamifiedcalendar/gamifiedcalendar/src/main/java/com/swe/gamifiedcalendar/Account.java
+++ Backend-Stuff/gamifiedcalendar/gamifiedcalendar/src/main/java/com/swe/gamifiedcalendar/Account.java
@@ -1,4 +1,88 @@
 package com.swe.gamifiedcalendar;
 
+import java.util.Objects;
+
 public class Account {
+    private int accountID;
+    private String username;
+    private String email;
+    private String password;
+    private int bestStreak;
+    private int currentStreak;
+    private int totalPoints;
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getBestStreak() {
+        return bestStreak;
+    }
+
+    public void setBestStreak(int bestStreak) {
+        this.bestStreak = bestStreak;
+    }
+
+    public int getCurrentStreak() {
+        return currentStreak;
+    }
+
+    public void setCurrentStreak(int currentStreak) {
+        this.currentStreak = currentStreak;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return  Objects.equals(getAccountID(), account.getAccountID()) &&
+                Objects.equals(getUsername(), account.getUsername()) &&
+                Objects.equals(getEmail(), account.getEmail()) &&
+                Objects.equals(getPassword(), account.getPassword()) &&
+                Objects.equals(getBestStreak(), account.getBestStreak()) &&
+                Objects.equals(getCurrentStreak(), account.getCurrentStreak()) &&
+                Objects.equals(getTotalPoints(), account.getTotalPoints());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAccountID(), getUsername(), getEmail(), getPassword(), getBestStreak(), getCurrentStreak(), getTotalPoints());
+    }
 }
